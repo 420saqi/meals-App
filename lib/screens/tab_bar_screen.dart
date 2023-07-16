5b599d8af34e4ab5cf6ca61e3e05c94c6e31bf49
saqib <saqi.saqib143@gmail.com>
2023-07-16 23:03:50 +0500

    tab bar with bottomNavigationBar done

--- lib/screens/tab_bar_screen.dart
+++ lib/screens/tab_bar_screen.dart
@@ -11,27 +11,67 @@ class TabsBarScreen extends StatefulWidget {
 }
 
 class _TabsBarScreenState extends State<TabsBarScreen> {
+
+  final tabBarItems=[
+    CategoriesScreen(),
+    FavouriteScreen(),
+  ];
+
+  int selectedIndex=0;
+  void selectedItem(index){
+    setState(() {
+      selectedIndex=index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        // initialIndex: 0, // default is 0 means categoriesScreen is selected
-        length: 2,
-        child: Scaffold(
-      appBar: AppBar(
-        title: Text('Deli Meals'),
-        bottom: TabBar(
-          tabs: [
-            Tab(icon: Icon(Icons.category),text: 'Category',),
-            Tab(icon: Icon(Icons.favorite),text: 'Favourite',)
-          ],
-        ),
-      ),
-      body: TabBarView(
-        children: [
-          CategoriesScreen(),
-          FavouriteScreen(),
+    return Scaffold(
+      body: tabBarItems[selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.white60,
+        selectedItemColor: Colors.white,
+        backgroundColor: Colors.deepPurple,
+        onTap: selectedItem,
+        currentIndex: selectedIndex,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.category,
+              ),
+              label: 'Categories'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+              ),
+              label: 'Favourites'),
         ],
       ),
-    ));
+    );
   }
 }
+
+// class _TabsBarScreenState extends State<TabsBarScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return DefaultTabController(
+//       // initialIndex: 0, // default is 0 means categoriesScreen is selected
+//         length: 2,
+//         child: Scaffold(
+//           appBar: AppBar(
+//             title: Text('Deli Meals'),
+//             bottom: TabBar(
+//               tabs: [
+//                 Tab(icon: Icon(Icons.category),text: 'Category',),
+//                 Tab(icon: Icon(Icons.favorite),text: 'Favourite',)
+//               ],
+//             ),
+//           ),
+//           body: TabBarView(
+//             children: [
+//               CategoriesScreen(),
+//               FavouriteScreen(),
+//             ],
+//           ),
+//         ));
+//   }
+// }
